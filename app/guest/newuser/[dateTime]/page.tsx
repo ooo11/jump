7c83f83d7cf740236acd2a5a08a986f08e757a07
ooo11 <markmizuro@gmail.com>
2024-03-10 00:23:43 +0800

    Update env

--- app/guest/newuser/[dateTime]/page.tsx
+++ app/guest/newuser/[dateTime]/page.tsx
@@ -3,20 +3,19 @@ import Form from '@/app/ui/guest/user-registration-form';
 import { Metadata } from 'next';
 
 interface PageProps {
-    query: {
-        dateTime: string;
-    };
+    params: { dateTime: string };
+    searchParams: { [key: string]: string | string[] | undefined };
 }
 
 export const metadata: Metadata = {
     title: 'Create New User',
 };
 
-export default async function Page({ query }: PageProps) {
+export default async function Page({ params, searchParams }: PageProps) {
     const [city] = await Promise.all([fetchCity()]);
 
     // Extract date and time from query parameters
-    const dateTime = query.dateTime;
+    const dateTime = params.dateTime;
 
     return (
         <div className="mt-5 flex w-full justify-center">
