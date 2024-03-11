36acc2b624d208b3be72326b4758cbedf8847297
ooo11 <markmizuro@gmail.com>
2024-03-11 16:13:47 +0800

    Rework css on package 💅🏻

--- app/guest/success/page.tsx
+++ app/guest/success/page.tsx
@@ -1,9 +1,11 @@
 
 import { Metadata } from 'next';
+import Link from 'next/link';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
 
 
 export const metadata: Metadata = {
-    title: 'Guest Homepage',
+    title: 'Guest Success',
 };
 
 
@@ -15,12 +17,14 @@ export default async function Page() {
 
     return (
 
-        <main>
-            <div className="mt-5 flex w-full justify-center">
-                <p>✨✨✨Your order is placed!✨✨✨</p>
-            </div>
-
-
+        <main className="flex flex-col justify-center items-center min-h-screen bg-teal-200">
+            <p className="text-xl text-gray-500 mb-6">✨✨✨Your order is placed!✨✨✨</p>
+            <Link
+                href="/guest"
+                className="rounded-lg bg-yellow-300 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            >
+                <span>See the Guest Homepage</span> <ArrowRightIcon className="w-5 md:w-6" />
+            </Link>
         </main>
 
     );
